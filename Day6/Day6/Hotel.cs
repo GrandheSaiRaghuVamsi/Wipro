ec9955460ee62e12a9c1c415d80539f58feb9851
Grandhe Sai Raghu Vamsi <139790743+GrandheSaiRaghuVamsi@users.noreply.github.com>
2025-02-06 18:31:47 +0530

    Add files via upload

---
+++ Day6/Day6/Hotel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class Hotel : IRoom<RoomAC> , IRoom<RoomNonAC>
+    {
+        List<RoomAC> lstrooms = new List<RoomAC>()
+        {
+            new RoomAC(){RoomNo=111,RoomType="Single",Price=5000},
+            new RoomAC(){RoomNo=113,RoomType="Single",Price=5000},
+            new RoomAC(){RoomNo=112,RoomType="Double",Price=8000},
+            new RoomAC(){RoomNo=114,RoomType="Single",Price=5000},
+
+        };
+
+        List<RoomNonAC> lstroomsn = new List<RoomNonAC>()
+        {
+            new RoomNonAC(){RoomNo=111,RoomType="Single",Price=30},
+            new RoomNonAC(){RoomNo=113,RoomType="Single",Price=20},
+            new RoomNonAC(){RoomNo=112,RoomType="Double",Price=40},
+            new RoomNonAC(){RoomNo=114,RoomType="Single",Price=30},
+
+        };
+
+        public void AddRoom(RoomAC item)
+        {
+            lstrooms.Add(item);
+            Console.WriteLine("Room added successfully" + item.RoomNo + " " + item.RoomType + " " + item.Price);
+        }
+
+        public void AddRoom(RoomNonAC itemn)
+        {
+            lstroomsn.Add(itemn);
+            Console.WriteLine("Room added successfully" + itemn.RoomNo + " " + itemn.RoomType + " " + itemn.Price);
+
+        }
+
+        IEnumerable<RoomNonAC> IRoom<RoomNonAC>.GetAll()
+        {
+            return lstroomsn;
+        }
+
+        IEnumerable<RoomAC> IRoom<RoomAC>.GetAll()
+        {
+            return lstrooms;
+        }
+    }
+}
