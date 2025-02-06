ddb19854651990ddc137f44026073f71b911d0f4
Grandhe Sai Raghu Vamsi <139790743+GrandheSaiRaghuVamsi@users.noreply.github.com>
2025-02-06 18:35:36 +0530

    Add files via upload

---
+++ DDay7/Day7/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public delegate void dels(string s);
+    public delegate void dels1();
+    private static void Main(string[] args)
+    {
+        Program o = new Program();
+        //Console.WriteLine("Hello, World!");
+        dels d = new dels(o.Disp);
+        d("Sai Raghu Vamsi Grandhe");
+
+        dels1 de = new dels1(Disp);
+        de();
+
+        Disp();
+        o.Disp("qwe");
+        
+    }
+     void Disp(string u)
+    {
+        Console.WriteLine(u);
+    }
+
+    static void Disp()
+    {
+        Console.WriteLine("HELLo EVERy ");
+    }
+}
